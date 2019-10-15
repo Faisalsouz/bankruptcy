4d75be4bc8c2a88dc931d8f6ad69c069e5396c2c
shrnkm <sahar.niknam@gmail.com>
2019-10-15 23:42:33 +0200

    add the pickles

--- data/Eikon/csvProcessing.py
+++ data/Eikon/csvProcessing.py
@@ -1,24 +1,21 @@
 import os
 import pandas as pd
-
+import pickle
 
 # read the Compustat-extracted lists of
 # healthy and bankrupt companies
 # ----------------------------------------------------------------------
 # set the path for reading the Compustat lists
-# path = 'D:\\studyproject\\bankruptcy\\data\\compustat\\' # for wind decomment this line
-path = '/Users/user/Documents/Bankruptcy/bankruptcy/data/compustat/' # for mac decomment this line
-# and read the files
-bankrupt = pd.read_csv(str(path + 'list_bankrupt.csv'), dtype=object)
-healthy = pd.read_csv(str(path + 'list_healthy.csv'), dtype=object)
+path = 'D:\\studyproject\\bankruptcy\\data\\compustat\\' # for win decomment this line
+# path = '/Users/user/Documents/Bankruptcy/bankruptcy/data/compustat/' # for mac decomment this line
 
 # set the path for reading the csv files
 # --------------- for win ---------------
-# path_b = 'convertedCSVfiles\\bankrupt\\'
-# path_h = 'convertedCSVfiles\\healthy\\'
+path_b = 'convertedCSVfiles\\bankrupt\\'
+path_h = 'convertedCSVfiles\\healthy\\'
 # --------------- for mac ---------------
-path_b = './convertedCSVfiles/bankrupt/'
-path_h = './convertedCSVfiles/healthy/'
+# path_b = './convertedCSVfiles/bankrupt/'
+# path_h = './convertedCSVfiles/healthy/'
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
 
@@ -26,6 +23,9 @@ path_h = './convertedCSVfiles/healthy/'
 # ----------------------------------------------------------------------
 # 1. Merging Bankrupt Companies Data
 # ----------------------------------------------------------------------
+
+# read the bankrupt list
+bankrupt = pd.read_csv(str(path + 'list_bankrupt.csv'), dtype=object)
 
 # remove the glitch column
 bankrupt = bankrupt.drop(['Unnamed: 0'], axis=1)
@@ -61,10 +61,17 @@ for i in range(len(bankrupt)):
         print('WARNING: There is inconsistency at row:', i)
 
 
+
+# ----------------------------------------------------------------------
 # attach the ISIN columns to the bankrupt dataframe
 bankrupt['csp2ISIN'] = bankrupt_csp2isn['ISIN']
 bankrupt['tic2ISIN'] = bankrupt_tic2isn['ISIN']
+# ----------------------------------------------------------------------
 
+
+# ----------------------------------------------------------------------
+# merge the two ISIN columns
+# ----------------------------------------------------------------------
 # add a single column for ISIN, and
 bankrupt['ISIN'] = ''
 # a column to indicate there was inconsistency in converting symbols
@@ -156,11 +163,17 @@ for i in range(len(bankrupt)):
     if bankrupt.iloc[i][4] != bankrupt_tic2ric.iloc[i][0]:
         print('WARNING: There is inconsistency at row:', i)
 
+
+# ----------------------------------------------------------------------
 # attach the RIC columns to the bankrupt dataframe
 bankrupt['csp2RIC'] = bankrupt_csp2ric['RIC']
 bankrupt['tic2RIC'] = bankrupt_tic2ric['RIC']
+# ----------------------------------------------------------------------
 
 
+# ----------------------------------------------------------------------
+# merge the two RIC columns
+# ----------------------------------------------------------------------
 # add a single column for RIC, and
 bankrupt['RIC'] = ''
 # a column to indicate there was inconsistency in converting symbols
@@ -230,86 +243,49 @@ bankrupt = bankrupt.drop(['csp2RIC', 'tic2RIC'], axis=1)
 # ----------------------------------------------------------------------
 # 2. Merging Healthy Companies Data
 # ----------------------------------------------------------------------
+# read the healthy list
+healthy = pd.read_csv(str(path + 'list_healthy.csv'), dtype=object)
+
 # remove the glitch column
 healthy = healthy.drop(['Unnamed: 0'], axis=1)
 
 # ----------------------------------------------------------------------
 ## 2.1. ISIN
 # ----------------------------------------------------------------------
-# work with ISIN codes converted from CUSIP
+# read and merge ISIN codes converted from CUSIP
 # ----------------------------------------------------------------------
-# read the healthy companies CUSIP to ISIN convert-table csv files
-healthy_csp2isn1 = pd.read_csv(path_h + 'healthy_csp2isn1.csv', dtype=object)
-healthy_csp2isn2 = pd.read_csv(path_h + 'healthy_csp2isn2.csv', dtype=object)
-healthy_csp2isn3 = pd.read_csv(path_h + 'healthy_csp2isn3.csv', dtype=object)
-healthy_csp2isn4 = pd.read_csv(path_h + 'healthy_csp2isn4.csv', dtype=object)
-healthy_csp2isn5 = pd.read_csv(path_h + 'healthy_csp2isn5.csv', dtype=object)
-healthy_csp2isn6 = pd.read_csv(path_h + 'healthy_csp2isn6.csv', dtype=object)
-
-# concatenate the dataframes
-healthy_csp2isn = pd.concat([healthy_csp2isn1, healthy_csp2isn2, healthy_csp2isn3,
-                             healthy_csp2isn4, healthy_csp2isn5, healthy_csp2isn6])
-
+# read the healthy companies CUSIP to ISIN conversion csv files
+healthy_csp2isn = [pd.read_csv(path_h + 'healthy_csp2isn{}.csv'.format(i+1), dtype=object) for i in range(6)]
+# merge them
+healthy_csp2isn = pd.concat([healthy_csp2isn[i] for i in range(6)])
+# clean the resulted dataframe
 healthy_csp2isn.rename(columns={'Unnamed: 0':'CUSIP', 'ISIN':'csp2ISIN'}, inplace=True)
+# and remove that extra uninformative column
 healthy_csp2isn = healthy_csp2isn.drop(['error'], axis=1)
-
-# check whether there are NAN values in CUSIP column
-# there should not be, cause it is already taken care of,
-# but still.... cause that's important and would be catastrophic
-# for merging results
-print(healthy_csp2isn.isnull().any()) # return False in case of no NANs
-print()
-print('The number of companies in the CUSIP to ISIN list is', len(healthy_csp2isn))
-
-# merge CUSIP-to-ISIN dataframe with the healthy list
-healthy = pd.merge(healthy, healthy_csp2isn, on=['CUSIP'], how='outer')
-
-# OCD-check whether the length of healthy dataframe is still 20783
-print()
-print(len(healthy))
-# OCD-check whether there are NAN values in 'Identifier' and 'Company' columns
-print()
-print(healthy.isnull().any())
-print('\n----------------------------------------------------------------------\n')
+# first-merge CUSIP-to-ISIN dataframe with the healthy list
+healthy = healthy.merge(healthy_csp2isn, how='left', on=['CUSIP'])
 
 # ----------------------------------------------------------------------
-# work with ISIN codes converted from Ticker
+# read and merge ISIN codes converted from Ticker
 # ----------------------------------------------------------------------
-# read the healthy companies Ticker to ISIN convert-table csv files
-healthy_tic2isn1 = pd.read_csv(path_h + 'healthy_tic2isn1.csv', dtype=object)
-healthy_tic2isn2 = pd.read_csv(path_h + 'healthy_tic2isn2.csv', dtype=object)
-healthy_tic2isn3 = pd.read_csv(path_h + 'healthy_tic2isn3.csv', dtype=object)
-healthy_tic2isn4 = pd.read_csv(path_h + 'healthy_tic2isn4.csv', dtype=object)
-healthy_tic2isn5 = pd.read_csv(path_h + 'healthy_tic2isn5.csv', dtype=object)
-healthy_tic2isn6 = pd.read_csv(path_h + 'healthy_tic2isn6.csv', dtype=object)
-
-# concatenate the dataframes
-healthy_tic2isn = pd.concat([healthy_tic2isn1, healthy_tic2isn2, healthy_tic2isn3,
-                             healthy_tic2isn4, healthy_tic2isn5, healthy_tic2isn6])
-
+# read the healthy companies Ticker to ISIN conversion csv files
+healthy_tic2isn = [pd.read_csv(path_h + 'healthy_tic2isn{}.csv'.format(i+1), dtype=object) for i in range(6)]
+# merge them
+healthy_tic2isn = pd.concat([healthy_tic2isn[i] for i in range(6)])
+# clean the resulted dataframe
 healthy_tic2isn.rename(columns={'Unnamed: 0':'Ticker', 'ISIN':'tic2ISIN'}, inplace=True)
+# and remove that extra uninformative column
 healthy_tic2isn = healthy_tic2isn.drop(['error'], axis=1)
+# second merge Ticker-to-ISIN dataframe with the first-merged healthy list
+healthy = healthy.merge(healthy_tic2isn, on=['Ticker'], how='left')
 
-# check whether there are NAN values in Ticker column
-# there should not be, cause it is already taken care of,
-# but still.... cause that's important and would be catastrophic
-# for merging results
-print(healthy_tic2isn.isnull().any()) # return False in case of no NANs
-print()
-print('The number of companies in the Ticker to CUSIP list is', len(healthy_tic2isn))
-
-# merge Ticker-to-ISIN dataframe with the healthy dataframe
-healthy = pd.merge(healthy, healthy_tic2isn, on=['Ticker'], how='outer')
-
-# OCD-check whether the length of healthy dataframe is still 20783
-print()
-print(len(healthy))
-# OCD-check whether there are NAN values in 'Identifier' and 'Company' columns
-print()
-print(healthy.isnull().any())
-print('\n----------------------------------------------------------------------\n')
+# ----------------------------------------------------------------------
 
 
+
+# ----------------------------------------------------------------------
+# merge the two ISIN columns
+# ----------------------------------------------------------------------
 # add a single column for ISIN, and
 healthy['ISIN'] = ''
 # a column to indicate there was inconsistency in converting symbols
@@ -381,81 +357,38 @@ healthy = healthy.drop(['csp2ISIN', 'tic2ISIN'], axis=1)
 ## 2.2. RIC
 
 # ----------------------------------------------------------------------
-# work with RIC codes converted from CUSIP
+# read and merge RIC codes converted from CUSIP
 # ----------------------------------------------------------------------
 # read the healthy companies CUSIP to RIC convert-table csv files
-healthy_csp2ric1 = pd.read_csv(path_h + 'healthy_csp2ric1.csv', dtype=object)
-healthy_csp2ric2 = pd.read_csv(path_h + 'healthy_csp2ric2.csv', dtype=object)
-healthy_csp2ric3 = pd.read_csv(path_h + 'healthy_csp2ric3.csv', dtype=object)
-healthy_csp2ric4 = pd.read_csv(path_h + 'healthy_csp2ric4.csv', dtype=object)
-healthy_csp2ric5 = pd.read_csv(path_h + 'healthy_csp2ric5.csv', dtype=object)
-healthy_csp2ric6 = pd.read_csv(path_h + 'healthy_csp2ric6.csv', dtype=object)
-
-# concatenate the dataframes
-healthy_csp2ric = pd.concat([healthy_csp2ric1, healthy_csp2ric2, healthy_csp2ric3,
-                             healthy_csp2ric4, healthy_csp2ric5, healthy_csp2ric6])
-
+healthy_csp2ric = [pd.read_csv(path_h + 'healthy_csp2ric{}.csv'.format(i+1), dtype=object) for i in range(6)]
+# merge them
+healthy_csp2ric = pd.concat([healthy_csp2ric[i] for i in range(6)])
+# clean the resulted dataframe
 healthy_csp2ric.rename(columns={'Unnamed: 0':'CUSIP', 'RIC':'csp2RIC'}, inplace=True)
+# and remove that extra uninformative column
 healthy_csp2ric = healthy_csp2ric.drop(['error'], axis=1)
-
-# check whether there are NAN values in CUSIP column
-# there should not be, cause it is already taken care of,
-# but still.... cause that's important and would be catastrophic
-# for merging results
-print(healthy_csp2ric.isnull().any()) # return False in case of no NANs
-print()
-print('The number of companies in the CUSIP to RIC list is', len(healthy_csp2ric))
-
-# merge CUSIP-to-RIC dataframe with the healthy list
-healthy = pd.merge(healthy, healthy_csp2ric, on=['CUSIP'], how='outer')
-
-# OCD-check whether the length of healthy dataframe is still 20783/20890
-print()
-print(len(healthy))
-# OCD-check whether there are NAN values in 'Identifier' and 'Company' columns
-print()
-print(healthy.isnull().any())
-print('\n----------------------------------------------------------------------\n')
+# first-merge CUSIP-to-RIC dataframe with the healthy list
+healthy = healthy.merge(healthy_csp2ric, how='left', on=['CUSIP'])
 
 # ----------------------------------------------------------------------
-# work with RIC codes converted from Ticker
+# read and merge RIC codes converted from Ticker
 # ----------------------------------------------------------------------
-# read the healthy companies Ticker to RIC convert-table csv files
-healthy_tic2ric1 = pd.read_csv(path_h + 'healthy_tic2ric1.csv', dtype=object)
-healthy_tic2ric2 = pd.read_csv(path_h + 'healthy_tic2ric2.csv', dtype=object)
-healthy_tic2ric3 = pd.read_csv(path_h + 'healthy_tic2ric3.csv', dtype=object)
-healthy_tic2ric4 = pd.read_csv(path_h + 'healthy_tic2ric4.csv', dtype=object)
-healthy_tic2ric5 = pd.read_csv(path_h + 'healthy_tic2ric5.csv', dtype=object)
-healthy_tic2ric6 = pd.read_csv(path_h + 'healthy_tic2ric6.csv', dtype=object)
-
-# concatenate the dataframes
-healthy_tic2ric = pd.concat([healthy_tic2ric1, healthy_tic2ric2, healthy_tic2ric3,
-                             healthy_tic2ric4, healthy_tic2ric5, healthy_tic2ric6])
-
+# read the healthy companies Ticker to RIC conversion csv files
+healthy_tic2ric = [pd.read_csv(path_h + 'healthy_tic2ric{}.csv'.format(i+1), dtype=object) for i in range(6)]
+# merge them
+healthy_tic2ric = pd.concat([healthy_tic2ric[i] for i in range(6)])
+# clean the resulted dataframe
 healthy_tic2ric.rename(columns={'Unnamed: 0':'Ticker', 'RIC':'tic2RIC'}, inplace=True)
+# and remove that extra uninformative column
 healthy_tic2ric = healthy_tic2ric.drop(['error'], axis=1)
-
-# check whether there are NAN values in Ticker column
-# there should not be, cause it is already taken care of,
-# but still.... cause that's important and would be catastrophic
-# for merging results
-print(healthy_tic2ric.isnull().any()) # return False in case of no NANs
-print()
-print('The number of companies in the Ticker to RIC list is', len(healthy_tic2ric))
-
-# merge Ticker-to-RIC dataframe with the healthy dataframe
-healthy = pd.merge(healthy, healthy_tic2ric, on=['Ticker'], how='outer')
-
-# OCD-check whether the length of healthy dataframe is still 20783/20890
-print()
-print(len(healthy))
-# OCD-check whether there are NAN values in 'Identifier' and 'Company' columns
-print()
-print(healthy.isnull().any())
-print('\n----------------------------------------------------------------------\n')
+# second merge Ticker-to-RIC dataframe with the first-merged healthy list
+healthy = healthy.merge(healthy_tic2ric, on=['Ticker'], how='left')
+# ----------------------------------------------------------------------
 
 
-
+# ----------------------------------------------------------------------
+# merge the two RIC columns
+# ----------------------------------------------------------------------
 # add a single column for RIC, and
 healthy['RIC'] = ''
 # a column to indicate there was inconsistency in converting symbols
@@ -523,7 +456,64 @@ healthy = healthy.drop(['csp2RIC', 'tic2RIC'], axis=1)
 
 
 
-
+# ----------------------------------------------------------------------
+# save the final lists of companies
+# ----------------------------------------------------------------------
 # save the final csv files of bankrupt and healthy companies
 bankrupt.to_csv('final_bankrupt_list.csv')
 healthy.to_csv('final_healthy_list.csv')
+
+
+# ----------------------------------------------------------------------
+# extract and save 4 lists of RICs and ISINs for bankrupt and healthy companies
+# ----------------------------------------------------------------------
+
+# create a folder to save the list-pickles
+os.mkdir('Lists')
+
+# ISIN list for bankrupt companies
+bankrupt_ISIN = bankrupt.ISIN[bankrupt['ISINc'] != 3].dropna().to_list()
+# handle serious conflict cases which include more than one code
+for index, row in bankrupt.iterrows():
+    if row['ISINc'] == 3:
+        bankrupt_ISIN.append(row['ISIN'][:row['ISIN'].find(' - ')])
+        bankrupt_ISIN.append(row['ISIN'][row['ISIN'].find(' - ') + 3:])
+# and save the list
+with open('Lists/bankrupt_ISIN.txt', 'wb') as f:
+    pickle.dump(bankrupt_ISIN, f)
+
+
+# create RIC list for bankrupt companies
+bankrupt_RIC = bankrupt.RIC[bankrupt['RICc'] != 3].dropna().to_list()
+# handle serious conflict cases which include more than one code
+for index, row in bankrupt.iterrows():
+    if row['RICc'] == 3:
+        bankrupt_RIC.append(row['RIC'][:row['RIC'].find(' - ')])
+        bankrupt_RIC.append(row['RIC'][row['RIC'].find(' - ') + 3:])
+# and save the list
+with open('Lists/bankrupt_RIC.txt', 'wb') as f:
+    pickle.dump(bankrupt_RIC, f)
+
+
+# ISIN list for healthy companies
+healthy_ISIN = healthy.ISIN[healthy['ISINc'] != 3].dropna().to_list()
+# handle serious conflict cases which include more than one code
+for index, row in healthy.iterrows():
+    if row['ISINc'] == 3:
+        healthy_ISIN.append(row['ISIN'][:row['ISIN'].find(' - ')])
+        healthy_ISIN.append(row['ISIN'][row['ISIN'].find(' - ') + 3:])
+# and save the list
+with open('Lists/healthy_ISIN.txt', 'wb') as f:
+    pickle.dump(healthy_ISIN, f)
+
+
+# RIC list for healthy companies
+healthy_RIC = healthy.RIC[healthy['RICc'] != 3].dropna().to_list()
+# handle serious conflict cases which include more than one code
+for index, row in healthy.iterrows():
+    if row['RICc'] == 3:
+        healthy_RIC.append(row['RIC'][:row['RIC'].find(' - ')])
+        healthy_RIC.append(row['RIC'][row['RIC'].find(' - ') + 3:])
+# and save the list
+with open('Lists/healthy_RIC.txt', 'wb') as f:
+    pickle.dump(healthy_RIC, f)
