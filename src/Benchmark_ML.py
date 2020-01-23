6908f3cdf59697341348672aabb07129852e6ae7
fatemehaa <50797701+fatemehaa@users.noreply.github.com>
2020-01-23 10:52:20 +0100

    Update Benchmark_ML.py

--- src/Benchmark_ML.py
+++ src/Benchmark_ML.py
@@ -1,67 +1,63 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-import collections
 from sklearn import metrics
 from collections import Counter
 # Import the model we are using
 from sklearn.ensemble import RandomForestClassifier
-from matplotlib import pyplot as plt
 from sklearn.metrics import precision_score, recall_score, roc_curve
-from imblearn.over_sampling import ADASYN
+#from imblearn.over_sampling import ADASYN
 from sklearn.linear_model import SGDClassifier
 from sklearn.svm import SVC
-from sklearn.linear_model import LinearRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-base_path = '/Users/Cora/Documents/UniOS/BP/'
+#base_path = '/Users/Cora/Documents/UniOS/BP/'
 
-df_bankrupt = pd.read_csv(base_path+'data_test_bankrupt.csv')
-df_healthy = pd.read_csv(base_path+'data_test_healthy.csv')
+df_mixed = pd.read_csv('data.csv')
+
 
 #print('\nExample Entry of a healthy company:\n', df_healthy.loc[0])
 
-df_mixed = pd.concat([df_bankrupt, df_healthy])
-temp_data = df_mixed.drop(['Instrument', 'Bankrupt'], axis=1)
+
 temp_labels = df_mixed['Bankrupt']
-temp_cik = df_mixed['Instrument']
+#temp_cik = df_mixed['Instrument']
+#temp_time = df_mixed['Year']
+temp_data = df_mixed.drop(['Bankrupt'], axis=1)
 
 
 labels = []
-cik = []
+#cik = []
 data = []
-data.extend((temp_data.values[0],temp_data.values[25], temp_data.values[51], temp_data.values[68]))
-labels.extend((temp_labels.values[1],temp_labels.values[26], temp_labels.values[52], temp_labels.values[69]))
+#data.extend((temp_data.values[0],temp_data.values[25], temp_data.values[51], temp_data.values[68]))
+#labels.extend((temp_labels.values[1],temp_labels.values[26], temp_labels.values[52], temp_labels.values[69]))
+print(temp_labels[17243:])
 
-
-
-# Extracts data and labels of year 2018 so we can predict 2019
-for i in range(81, len(df_mixed.index), 20):
-    labels.append(temp_labels.values[i+1])
-    cik.append(temp_cik.values[i])
+# Extracts data and labels of year 5 so we can predict the 7th
+for i in range(0, len(df_mixed.index), 5):
+    labels.append(temp_labels.values[i])
     data.append(temp_data.values[i])
 
 # Convert to numpy array
 data = np.array(data)
 labels = np.array(labels)
-print('\nExample Entry of data:\n', data[196])  # 197 Samples
-#ada = ADASYN(ratio={1: 4, 0: 193}, n_neighbors = 4)
+#print('\nExample Entry of data:\n', data[196])
 print('Original dataset shape %s' % Counter(labels))
-X_res, y_res = ADASYN(n_neighbors = 3).fit_sample(data, labels)
+# resample to get more bankrupt samples
+#X_res, y_res = ADASYN(n_neighbors = 5, random_state = 0).fit_sample(data, labels)
 
-print('Resampled dataset shape %s' % Counter(y_res))
 
-train_data, test_data, train_labels, test_labels = train_test_split(X_res, y_res,
+#print('Resampled dataset shape %s' % Counter(y_res))
+
+#train_data, test_data, train_labels, test_labels = train_test_split(X_res, y_res,
+ #                                                                   test_size = 0.25,
+ #                                                                   random_state = 0)
+
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels,
                                                                     test_size = 0.25,
                                                                     random_state = 0)
-#print('Training Data Shape:', train_data.shape)
-#print('Training Labels Shape:', train_labels.shape)
-#print('Testing data Shape:', test_data.shape)
-#print('Testing Labels Shape:', test_labels.shape)
 
-# Check number of bankrupt companies in test data
-#print('Number of healthy and bankrupt in test data: ', collections.Counter(labels))
-
+# Random forests
 def rf(x_train, y_train, x_test):
     # Instantiate model with 1000 decision trees
     # Create a Gaussian Classifier
@@ -71,26 +67,30 @@ def rf(x_train, y_train, x_test):
     label_pred = crf.predict(x_test)
 
     # Model Accuracy, how often is the classifier correct?
-    print("Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
-    print("Precision:", metrics.precision_score(test_labels, label_pred, 1))
+    print("Forest Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
+    print("Forest Precision:", metrics.precision_score(test_labels, label_pred, 1))
+    print("Forest ROC:", metrics.roc_curve(test_labels, label_pred, 1))
 
+# Support Vector Machine
 def svm(x_train, y_train, x_test):
     csvm = SVC(kernel='linear')
     csvm.fit(x_train, y_train)
     label_pred = csvm.predict(x_test)
     # Model Accuracy, how often is the classifier correct?
-    print("Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
-    print("Precision:", metrics.precision_score(test_labels, label_pred, 1))
-
-def LinReg(x_train, y_train, x_test):
-    clf = SGDClassifier()
+    print("SVM Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
+    print("SVM Precision:", metrics.precision_score(test_labels, label_pred, 1))
+    print("SVM ROC:", metrics.roc_curve(test_labels, label_pred, 1))
+# Linear Regression
+def linReg(x_train, y_train, x_test):
+    clr = SGDClassifier()
     # fit (train) the classifier
-    clf.fit(x_train, y_train)
-    label_pred = clf.predict(x_test)
+    clr.fit(x_train, y_train)
+    label_pred = clr.predict(x_test)
     # Model Accuracy, how often is the classifier correct?
-    print("Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
-    print("Precision:", metrics.precision_score(test_labels, label_pred, 1))
-    
+    print("Lin Reg Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
+    print("Lin Reg Precision:", metrics.precision_score(test_labels, label_pred, 1))
+    print("Lin Reg ROC:", metrics.roc_curve(test_labels, label_pred, 1))
+# MDA
 def mda(x_train, y_train, x_test):
     cmda = LinearDiscriminantAnalysis()
     cmda.fit(x_train, y_train)
@@ -98,8 +98,9 @@ def mda(x_train, y_train, x_test):
     # Model Accuracy, how often is the classifier correct?
     print("MDA Accuracy:", metrics.accuracy_score(test_labels, label_pred, 1))
     print("MDA Precision:", metrics.precision_score(test_labels, label_pred, 1))
+    print("MDA ROC:", metrics.roc_curve(test_labels, label_pred, 1))
 
-rf(train_data, train_labels, test_data)
-svm(train_data, train_labels, test_data)
-LinReg(train_data, train_labels, test_data)
 mda(train_data, train_labels, test_data)
+svm(train_data, train_labels, test_data)
+linReg(train_data, train_labels, test_data)
+rf(train_data, train_labels, test_data)
